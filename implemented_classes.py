3d0232a48600eb6a2e27336128cc82b3425e97e2
Mathias Øgaard <mapiza00@gmail.com>
2024-05-06 18:41:08 +0200

    Implemented Recorder

--- implemented_classes.py
+++ implemented_classes.py
@@ -1,21 +1,66 @@
-from meta_classes import MetaSpeechRecognizer, MetaEyeTracker, MetaScreen
+from meta_classes import MetaSpeechRecognizer, MetaRecorder, MetaEyeTracker, MetaScreen
 import speech_recognition as sr
+from audio_recorder2 import AudioRecorder
+from audio_recording_display import init_recorder, recognize
+import time
+import os
+
+
+
+class Recorder(MetaRecorder):
+    def __init__(self):
+        root_dir = os.path.dirname(os.path.abspath(__file__))
+        self.audio_file = root_dir + 'audio_recording.wav'
+        self.recorder = AudioRecorder(self.audio_file)
+
+    def start_listening(self):
+        init_recorder(self.recorder)
+        self.is_listening = True
+        #TODO: Remove print-statement ones done testing
+        print("Recorder initialized")
+
+    def stop_listening(self):
+        while (self.is_listening):
+            time.sleep(0.05)
+        #TODO: Remove print-statement ones done testing
+        print("Recorder stopped listening")
+        
+
+    def is_listening(self):
+        return self.recorder.is_started
+    
+    def has_audio(self):
+        return os.path.exists(self.audio_file)
+    
+    def get_audio(self):
+        assert self.has_audio(), "Couldn't get audio because the audio file didn't exist."
+        pass
+    
+    def clear(self):
+        """Deletes the 'audio_recording.wav' file."""
+        os.remove(self.audio_file)
+
 
 class SpeechRecognizer(MetaSpeechRecognizer):
 
     def __init__(self):
-        self.recorder = 
+        self.recorder = AudioRecorder()
         self.recognizer = sr.Recognizer()
+        self.message
 
-    def start_listening():
-        pass
+    def start_listening(self):
+        init_recorder(self.recorder)
+        #TODO: Remove print-statement ones done testing
+        print("Recorder initialized")
 
     def stop_listening():
         pass
 
-    def get_message():
-        pass
+    def get_message(self):
+        return self.message
 
     def is_listening():
         pass
-    
+
+class PositionTracker(MetaEyeTracker):
+    pass
--- meta_classes.py
+++ meta_classes.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-class MetaSpeechRecognizer(ABC):
+class MetaSpeechRec(ABC):
     """Meta class for speech recognizers.\n
     Methods:
     - __init__()
@@ -29,38 +29,105 @@ class MetaSpeechRecognizer(ABC):
     def is_listening(self):
         """Boolean - True if recognizer is listening."""
 
+class MetaRecorder(ABC):
+    """Abstract class for audio recorders.\n
+    A MetaRecorder should be able to record an audio message, and store it as an audio file.\n
+    Methods:
+    - __init__()
+    - start_listening()
+    - stop_listening()
+    - is_listening()
+    - get_audio()
+    - has_audio()"""
+
+    @abstractmethod
+    def __init__(self):
+        """Initializes the audio recoder."""
+
+    @abstractmethod
+    def start_listening(self):
+        """Should set the audio recorder in listening mode."""
+
+    @abstractmethod
+    def stop_listening(self):
+        """Should make the audio recorder stop listening."""
+
+    @abstractmethod
+    def is_listening(self):
+        """Boolean - True if recorder is listening."""
+        
+    @abstractmethod
+    def get_audio(self):
+        """Should return the recorded message."""
+
+    @abstractmethod
+    def has_audio(self):
+        """Boolean - True if the recorder has stored an audio file."""
+
+class MetaSpeechRecognizer(ABC):
+    """Meta class for speech recognizers.\n
+    A MetaSpeechRecognizer should be able to recognize speech from audio files.\n
+    Methods:
+    - __init__()
+    - recognize_audio()
+    - get_message()
+    - is_recognizing()
+    - has_recognized_message()"""
+
+    @abstractmethod
+    def __init__(self):
+        """Initializes the speech recognizer."""
+
+    @abstractmethod
+    def recognize_audio(self, audio):
+        """Recognizes speech from the given audio file."""
+
+    @abstractmethod
+    def get_message(self):
+        """Returns the recognized message."""
+
+    @abstractmethod
+    def is_recognizing(self):
+        """Boolean - True if recognizer is in the process of recognizing."""
+
+    @abstractmethod
+    def has_recognized_message(self):
+        """Boolean - True if recognizer has recognized a spoken message."""
+
+    
 class MetaEyeTracker(ABC):
     """Meta class for eye trackers.\n
     Methods:
     - __init__()
     - calibrate()
-    - get_pos()
+    - get_quadrant()
     - start_tracking()
     - stop_tracking()
     - is_tracking()"""
 
     @abstractmethod
-    def __init__():
+    def __init__(self):
         """Initializes the eye tracker."""
 
     @abstractmethod
-    def calibrate():
+    def calibrate(self):
         """Calibrates the eye tracker."""
 
     @abstractmethod
-    def get_pos():
-        """Returns the position of the eye gaze."""
+    def get_quadrant(self):
+        """Returns the quadrant the user is looking at.\n
+        Either top-left, top-right, bottom-left or bottom-right."""
 
     @abstractmethod
-    def start_tracking():
+    def start_tracking(self):
         """Should set the eye tracker in tracking mode."""
 
     @abstractmethod
-    def stop_tracking():
+    def stop_tracking(self):
         """Should make the eye tracker stop tracking."""
 
     @abstractmethod
-    def is_tracking():
+    def is_tracking(self):
         """Boolean - True if the position is tracked"""
 
 class MetaScreen(ABC):
@@ -69,30 +136,41 @@ class MetaScreen(ABC):
     - __init__()
     - get_objects()
     - draw_object()
+    - display_quadrants()
+    - highlight_quadrant()
     - show()
     - terminate()"""
 
     @abstractmethod
-    def __init__():
+    def __init__(self):
         """Initializes the eye gazer."""
 
     @abstractmethod
-    def get_objects():
+    def get_objects(self):
         """Returns a list of tuples, containing all drawn objects, together with their current location.\n
         Example:
         - [ ('red circle', (x, y)) , ('green square', (32, 45)) ]"""
 
     @abstractmethod
-    def draw_object(shape, color):
+    def draw_object(self, shape, color):
         """Draws an object with the given shape and color.\n
         The new object should be added to the screen's register of drawn objects,\n
         so that it can be found by calling get_objects()."""
 
     @abstractmethod
-    def show():
+    def display_quadrants(self):
+        """Displays a two by two grid, dividing the screen into four quadrants."""
+
+    @abstractmethod
+    def highlight_quadrant(self, quadrant):
+        """Highlights the given quadrant on the screen.
+        - quadrant should be a tuple (i,j), indicating the quadrant's coordinates"""
+
+    @abstractmethod
+    def show(self):
         """Updates the display of the screen."""
         
     @abstractmethod
-    def terminate():
+    def terminate(self):
         """Stop displaying the screen."""
 
